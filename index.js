9cd10560864dcd1441c6c444b1f5835ae83667f1
leela khadka <khadkaleela3@gmail.com>
2022-08-14 22:44:18 -0700

    commit tree 6722309f02e26573e592ab2f806cca1fb5a671a8

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Write your solution in this file!
+var customerName = 'bob';
+
+function window.customerName() {
+    return = customerName;
+}
+
+function upperCaseCustomerName() {
+    return upperCaseCustomerName + customerName;
+}
+
+
+
+
+
+
+
+
+
